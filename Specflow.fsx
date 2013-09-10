5bdc67e39d2768601db3878cf063a54f4bfb236e
mavnn <michael@mavnn.co.uk>
2013-09-10 12:09:36 +0100

    Generate SpecFlow report even if the unit tests fail.

--- Specflow.fsx
+++ Specflow.fsx
@@ -5,25 +5,32 @@ open Fake
 open Utils
 open System
 
+let private generateSpecFlowReport (config : Map<string, string>) =
+    SpecFlow
+        (fun defaults ->
+            { defaults with
+                ToolPath = config.get "core:tools" @@ specFlowRunners
+                SubCommand = "nunitexecutionreport"
+                ProjectFile = !! @".\**\*.Features.csproj" |>Seq.head
+                XmlTestResultFile = "SpecFlowResult.xml"
+                OutputFile = "SpecFlowResult.html"
+             })
+
 let run (config : Map<string, string>) _ =
     let testDlls = !! (sprintf @".\**\bin\%s\**\*.Features.dll" (config.get "build:configuration"))
     if Seq.length testDlls > 0 then
         ensureNunitRunner config
         ensureSpecFlowRunner config
-        testDlls
-        |> NUnit 
-            (fun defaults ->
-                { defaults with 
-                    ToolPath = config.get "core:tools" @@ nunitRunners
-                    OutputFile = "SpecFlowResult.xml"
-                 })
-        SpecFlow
-            (fun defaults ->
-                { defaults with
-                    ToolPath = config.get "core:tools" @@ specFlowRunners
-                    SubCommand = "nunitexecutionreport"
-                    ProjectFile = !! @".\**\*.Features.csproj" |>Seq.head
-                    XmlTestResultFile = "SpecFlowResult.xml"
-                    OutputFile = "SpecFlowResult.html"
-                 })
-
+        try
+            testDlls
+            |> NUnit 
+                (fun defaults ->
+                    { defaults with 
+                        ToolPath = config.get "core:tools" @@ nunitRunners
+                        OutputFile = "SpecFlowResult.xml"
+                     })
+        with
+        | e ->
+            generateSpecFlowReport config
+            raise e
+        generateSpecFlowReport config
