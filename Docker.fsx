ba763410794460b79105d767f2e9c2a92f3ec9c1
kjnilsson <kjnilsson@gmail.com>
2015-01-07 10:44:46 +0000

    docker build script improvements

--- Docker.fsx
+++ Docker.fsx
@@ -13,73 +13,44 @@ let run script dir =
     tracefn "docker: running %s" script
     if File.Exists (dir @@ script) then
         sprintf "running script %s" (dir @@ script) |> trace
-        ExecProcess (fun psi ->
+        let result = ExecProcess (fun psi ->
                         psi.FileName <- "/bin/bash" 
                         psi.Arguments <- dir @@ script
                         psi.WorkingDirectory <- dir) (TimeSpan.MaxValue)
-    else 0
+        if result <> 0 then failwith (sprintf "failure executing %s" script)
 
-let private updateDockerFile dir =
-    tracefn "docker: updating docker file in directory: %s" dir
-    let dockerFile = dir @@ "Dockerfile"
-    let dockerFileOrig = dir @@ "Dockerfile.orig"
-    if File.Exists(dockerFileOrig) then
-        File.Delete dockerFileOrig
-    File.Copy(dockerFile, dockerFileOrig)
-    let file = File.ReadAllText (dockerFile)
-    file.Replace(@"[[HACK]]", string DateTime.Now.Ticks)
-    |> fun s -> File.WriteAllText(dockerFile, s)
-    { new IDisposable with
-        member x.Dispose() =
-            File.Delete(dockerFile)
-            File.Move(dockerFileOrig, dockerFile) }
-
+let private execProcess fileName args wd =
+    let result = ExecProcess (fun psi ->
+                    psi.FileName <- fileName 
+                    psi.Arguments <- args 
+                    psi.WorkingDirectory <- wd) (TimeSpan.FromHours 1.0)
+    if result <> 0 then 
+        failwith (sprintf "ExecProcess failed (code: %i) for %s %s" result fileName args)
+       
 let private buildImage (config: Map<string, string>) name dir =
-    use x = updateDockerFile dir
-    let pre = run "pre.sh" dir
+    run "pre.sh" dir
     tracefn "docker: running Dockerfile in: %s" dir
     let image = sprintf "%s/%s:%s" (config.get "docker:registry") name (config.get "versioning:build")
-    let result = ExecProcess (fun psi ->
-                    psi.FileName <- "docker"
-                    psi.Arguments <- sprintf "build -t %s %s" image dir
-                    psi.WorkingDirectory <- dir) (TimeSpan.FromHours 1.0)
+    execProcess "docker" (sprintf "build -t %s %s" image dir) dir
     tracefn "docker: tagging with latest: %s" dir
     let registry = (config.get "docker:registry")
     let latest = sprintf "%s/%s:latest" registry name
-    let tag = ExecProcess (fun psi ->
-                    psi.FileName <- "docker"
-                    psi.Arguments <- sprintf "tag %s/%s:%s %s" registry name (config.get "versioning:build") latest
-                    psi.WorkingDirectory <- dir) (TimeSpan.FromHours 1.0)
-    let push =
-        if config.ContainsKey "docker:registry"  then
-            tracefn "docker: pushing: %s" image
-            let res = ExecProcess (fun psi ->
-				psi.FileName <- "docker"
-				psi.Arguments <- sprintf "push %s" image
-				psi.WorkingDirectory <- dir) (TimeSpan.FromHours 1.0)
-            tracefn "docker: pushing: %s" image
-            let res2 = ExecProcess (fun psi ->
-				psi.FileName <- "docker"
-				psi.Arguments <- sprintf "push %s" latest
-				psi.WorkingDirectory <- dir) (TimeSpan.FromHours 1.0)
-            res + res2
-        else 
-            trace "docker: config key [docker:registry] not found. skipping docker push"
-            0
-    let post = run "post.sh" dir
-    result + tag + pre + post + push, image
+    execProcess "docker" (sprintf "tag %s/%s:%s %s" registry name (config.get "versioning:build") latest) dir
+    if config.ContainsKey "docker:registry"  then
+        tracefn "docker: pushing: %s" image
+        execProcess "docker" (sprintf "push %s" image) dir
+        tracefn "docker: pushing: %s" image
+        execProcess "docker" (sprintf "push %s" latest) dir
+    else 
+        trace "docker: config key [docker:registry] not found. skipping docker push"
+    run "post.sh" dir
 
 
 let dockerize (config: Map<string, string>) _ =
     Directory.EnumerateDirectories (currentDir @@ "docker")
     |> Seq.map (fun d -> d, DirectoryInfo(d).Name)
-    |> Seq.map (fun (dir, name) ->
-        name, buildImage config name dir)
-    |> Seq.filter (fun (_,(res, image)) -> res > 0)
-    |> Seq.toList
-    |> function
-       | [] -> ()
-       | failed -> failwith "some docker images failed to generate %A" failed
+    |> Seq.iter (fun (dir, name) ->
+        buildImage config name dir)
 
 (*
 let config = Map ["versioning:build", "1235"; ]
