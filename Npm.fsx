20d85c2c1b093e5664270ae5a376e0a7b5bd2a4d
Martyn Osborne <martyn.osborne@15below.com>
2015-08-03 15:56:22 +0100

    crude gulp support

---
+++ Npm.fsx
@@ -0,0 +1,25 @@
+#r    @"../../../packages/FAKE/tools/FakeLib.dll"
+#load "./Utils.fsx"
+
+open Fake
+open Utils
+open System
+open System.IO
+
+let nodeDir64 = "C:\\Program Files\\nodejs\\"
+let nodeDir86 = "C:\\Program Files (x86)\\nodejs\\"
+let nodeDir = match Directory.Exists nodeDir64 with | true -> nodeDir64 | _ -> nodeDir86
+let node = Path.Combine(nodeDir, "node.exe")
+let npm = Path.Combine(nodeDir, "npm.cmd")
+
+let install (config : Map<string, string>) _ =
+
+    let args = "install"
+
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- npm
+            info.WorkingDirectory <- ".\\build"
+            info.Arguments <- args) (TimeSpan.FromMinutes 5.)
+
+    ()
