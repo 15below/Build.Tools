90d5f5c56669a4b612e1a4c06b3b034b157f9562
Martyn Osborne <zzdtri@live.co.uk>
2016-02-15 16:42:55 +0000

    add nunit3 runner support

--- Test.fsx
+++ Test.fsx
@@ -2,11 +2,20 @@
 #load "./Utils.fsx"
 
 open Fake
+open Fake.Testing
 open Utils
 open System
+open System.IO
 
 let run (config : Map<string, string>) _ =
     let testDlls = !! (sprintf @".\**\bin\%s\**\*.Tests.dll" (config.get "build:configuration"))
+    let nunitRunner = !! @".\**\nunit*-console.exe" |> Seq.head 
+        
     if Seq.length testDlls > 0 then
-        testDlls
-        |> NUnit (fun p -> { p with ToolPath = "packages/NUnit.Runners/tools" })
+        match nunitRunner |> Path.GetFileName with
+        | "nunit3-console.exe" ->
+            testDlls
+            |> NUnit3 (fun p -> { p with ToolPath = nunitRunner })
+        | _ -> 
+            testDlls
+            |> NUnit (fun p -> { p with ToolPath = "packages/NUnit.Runners/tools" })
